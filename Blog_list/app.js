a8db4ce8ee3caad5d144e425ca445ad1cf951110
Larisa Pyykölä <Larisa.pyykola@gmail.com>
2019-10-24 18:43:28 +0300

    utils added

--- Blog_list/app.js
+++ Blog_list/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -9,8 +9,8 @@ const blogsRouter = require('./controllers/blogs')
 
 
 const mongoUrl = process.env.MONGODB_URI
-console.log('connecting to', mongoUrl)
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(bodyParser.json())
